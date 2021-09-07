48ceafebcd7f9e5748cb08ef020d369d2b71fd6d
Valentin <valkjavalkja@gmail.com>
2021-09-07 18:19:18 -0300

    infl charts

--- src/components/EconomicHistory.jsx
+++ src/components/EconomicHistory.jsx
@@ -4,24 +4,31 @@ import {Line} from 'react-chartjs-2'
 
 export const EconomicHistory = () => {
     
-    const [chartData, setChartData] = useState ({})
-
+    const [firstMidCentury, setFirstMidCentury] = useState ({})
+    const [SecondMidCentury, setSecondMidCentury] = useState ({})
     let chartYears = []
+    let chartYears2 = []
 
     const historicInflation = ()=>{
         let year = 1900
-        for (year; year < 1990; year++){
+        for (year; year < 1956; year++){
             chartYears = chartYears.concat(year)
-    //    console.log(chartYears)
-    } 
+      } 
+} 
+const historicInflation2 = ()=>{
+    let year2 = 1956
+    for (year2; year2 < 2001; year2++){
+        chartYears2 = chartYears2.concat(year2)
+  } 
 } 
 
 historicInflation() 
 let yearsList = chartYears.map(years =>  (years))
-  console.log(yearsList)
+historicInflation2() 
+let yearsList2 = chartYears2.map(years =>  (years))
 
     const chart = () => {
-        setChartData({
+        setFirstMidCentury({
             labels: yearsList,
             datasets:[
                 {
@@ -31,9 +38,24 @@ let yearsList = chartYears.map(years =>  (years))
                           25,-10,-15,0,1,-1,-1,0,0,1,
                           1,-14,-10,11,-11,5,8,4,0,1,
                           2,3,5,6,1,0,20,19,30,25,
-                          30,40,49,5,5,10,15,25,30,110,
+                          30,40,49,5,5,10],
+                    backgroundColor:['rgba(75,123,324,0.6)'],
+                    borderWidth: 4
+                }
+            ]
+        })
+
+        setSecondMidCentury({
+            labels: yearsList2,
+            datasets:[
+                {
+                    label: 'inflation',
+                    data: [15,25,30,110,
                           25,10,26,24,23,25,27,26,15,8,
-                          10,45,55,56,30,170,448,165,165,160],
+                          10,45,55,56,30,170,448,165,165,160,
+                          105,110,170,350,451,451,99,148,348,3079,
+                          2314,175,40,15,8,2,1,1,1,-1 ,
+                          0,-1,48,5,8,10,9,8,7],
                     backgroundColor:['rgba(75,123,324,0.6)'],
                     borderWidth: 4
                 }
@@ -45,13 +67,37 @@ let yearsList = chartYears.map(years =>  (years))
         chart()
     }, [])
     
+
+    
     return (
         <div>
 
             <h1>econ history</h1>
 
+
             <div className="inflation">
-                <Line data={chartData} options={{
+                <Line data={firstMidCentury} options={{
+                    responsive:true,
+                    title: {text: 'inflation', display:true},
+                    scales:{
+                        yAxes: [
+                            {
+                                ticks:{
+                                    autoSkip:true,
+                                    maxTicksLimit:2,
+                                    beginAtZero:true
+                                },
+                                gridLines:{
+                                    display:false,
+                                }
+                            }
+                        ]
+                    }
+                }}/>
+            </div>
+
+            <div className="inflation2">
+                <Line data={SecondMidCentury} options={{
                     responsive:true,
                     title: {text: 'inflation', display:true},
                     scales:{
