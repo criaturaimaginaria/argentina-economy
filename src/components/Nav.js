ffa81dd7ca7f961c2c83d41f647874c7d3ed0d9e
Valentin <valkjavalkja@gmail.com>
2021-09-07 10:41:59 -0300

    toggle true & false

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './Nav.css';
+import './SlideNav.css';
 import {Link} from 'react-router-dom'
 
 
 export const Nav = () => {
+
+        const [toggle, setToggle] = useState (false)
+        
+
+        useEffect(()=>{
+          console.log(toggle) 
+        });
+
+        const toggleState = () => {
+            setToggle(!toggle)
+        }
+
     return (
         <nav>
+
+            <button onClick={toggleState }>hola</button>
+
             <ul className="nav-list">
                 <Link to='/components/Main'>
                     <li>Main</li>
