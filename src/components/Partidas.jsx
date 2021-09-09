796d58ec4fc822165e4afb5f2e58f67b695f5005
Valentin <valkjavalkja@gmail.com>
2021-09-09 10:08:39 -0300

    recharts

--- src/components/Partidas.jsx
+++ src/components/Partidas.jsx
@@ -1,10 +1,85 @@
-import React from 'react'
+
+import React from 'react';
+import  "./Partidas.css"
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Area, AreaChart, fill } from 'recharts';
+
+
 
 export const Partidas = () => {
+    const data = [
+        {
+          name: 'Page A',
+          uv: 4000,
+          pv: 2400,
+          amt: 2400,
+        },
+        {
+          name: 'Page B',
+          uv: 3000,
+          pv: 1398,
+          amt: 2210,
+        },
+        {
+          name: 'Page C',
+          uv: 2000,
+          pv: 9800,
+          amt: 2290,
+        },
+        {
+          name: 'Page D',
+          uv: 2780,
+          pv: 3908,
+          amt: 2000,
+        },
+        {
+          name: 'Page E',
+          uv: 1890,
+          pv: 4800,
+          amt: 2181,
+        },
+        {
+          name: 'Page F',
+          uv: 2390,
+          pv: 3800,
+          amt: 2500,
+        },
+        {
+          name: 'Page G',
+          uv: 3490,
+          pv: 4300,
+          amt: 2100,
+        },
+      ];
     return (
         <div>
             <h1>partidas</h1>
-            
+
+            <div className="partidas"> 
+                <ResponsiveContainer width="100%" aspect={2}>
+                    <LineChart
+                    
+                        width={500}
+                        height={300}
+                        data={data}
+                        margin={{
+                            top: 5,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                        > 
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Area type="monotone" dataKey="uv" stroke="black" fill="#82ca9d"/>
+                        <Tooltip />
+                        <Legend />
+                        <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} fill="#82ca9d"/>
+                        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+                    
+                    </LineChart>
+                </ResponsiveContainer>
+            </div>       
         </div>
     )
 }
